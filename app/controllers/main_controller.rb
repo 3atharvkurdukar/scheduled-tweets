d1c8ba36da0d97084f6bdc75abb92ce22368e116
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2021-11-17 21:52:05 +0530

    Added flash message alerts system

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,12 @@
 class MainController < ApplicationController
   def index
+
+    # The flash will persist through one extra request, before vanishing
+
+    flash[:info] = "You have logged in successfully!"
+
+    # With flash.now, the flash will be gone on next request
+
+    flash.now[:danger] = "Invalid configuration"
   end
 end
