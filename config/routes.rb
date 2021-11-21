1bb03a1a9a9706ee4bd270f37f552a1d7dfa2388
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2021-11-21 19:07:52 +0530

    Added change password functionality

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   post "/sign-up", to: "registrations#create"
   get "/sign-in", to: "sessions#new"
   post "/sign-in", to: "sessions#create"
+  get "/change-password", to: "passwords#edit"
+  patch "/change-password", to: "passwords#update"
   delete "/logout", to: "sessions#destroy"
 end
