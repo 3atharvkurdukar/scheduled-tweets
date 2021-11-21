db20521eb1b268a39d0eecf790c0c774bbf23a8c
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2021-11-21 13:57:42 +0530

    Added login form

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get "/about-us", to: "about#index", as: :about
   get "/sign-up", to: "registrations#new"
   post "/sign-up", to: "registrations#create"
+  get "/sign-in", to: "sessions#new"
+  post "/sign-in", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 end
