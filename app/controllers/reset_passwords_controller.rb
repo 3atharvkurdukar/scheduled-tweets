45667259cf50522047f38cc81b062cf72c2ea034
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2021-11-22 16:48:22 +0530

    Added reset password logic

--- app/controllers/reset_passwords_controller.rb
+++ app/controllers/reset_passwords_controller.rb
@@ -11,6 +11,21 @@ class ResetPasswordsController < ApplicationController
     redirect_to root_path, flash: { info: "A link to reset password has been sent to your email, if the account exists" }
   end
 
+  def edit
+    @user = User.find_signed!(params[:token], purpose: "reset_password")
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to sign_in_path, flash: { danger: "Your token has expired. Please try again!" }
+  end
+
+  def update
+    @user = User.find_signed!(params[:token], purpose: "reset_password")
+    if @user.update(password_params)
+      redirect_to sign_in_path, flash: { info: "Password reset successfully! Sign in again." }
+    end
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to sign_in_path, flash: { danger: "Your token has expired. Please try again!" }
+  end
+
   private
 
   def password_params
